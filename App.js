1cf88bfc0e7022aa3b8a4197fd6030c988b248f1
Rohan Bimal Raj <rohanbimalraj@gmail.com>
2024-04-26 11:10:30 +0530

    Added app colors

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Colors from './constants/colors';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{color: Colors.accent500}}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: Colors.primary700,
     alignItems: 'center',
     justifyContent: 'center',
   },
