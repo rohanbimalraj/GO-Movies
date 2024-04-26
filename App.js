8122860e6e300ca05dd0ad21cbae40cff2f0babe
Rohan Bimal Raj <rohanbimalraj@gmail.com>
2024-04-26 12:07:25 +0530

    Added Space Grotesk font

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Colors from './constants/colors';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  SpaceGrotesk_300Light,
+  SpaceGrotesk_400Regular,
+  SpaceGrotesk_500Medium,
+  SpaceGrotesk_600SemiBold,
+  SpaceGrotesk_700Bold,
+} from "@expo-google-fonts/space-grotesk";
+import Colors from "./constants/colors";
+import AppFonts from "./constants/app.fonts";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    SpaceGrotesk_300Light,
+    SpaceGrotesk_400Regular,
+    SpaceGrotesk_500Medium,
+    SpaceGrotesk_600SemiBold,
+    SpaceGrotesk_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <View></View>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={{color: Colors.accent500}}>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: Colors.accent500, fontFamily: AppFonts.SG_Bold, fontSize: 24 }}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +38,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.primary700,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ constants/app.fonts.js
@@ -0,0 +1,9 @@
+const AppFonts = {
+  SG_Light: "SpaceGrotesk_300Light",
+  SG_Regular: "SpaceGrotesk_400Regular",
+  SG_Medium: "SpaceGrotesk_500Medium",
+  SG_SemiBold: "SpaceGrotesk_600SemiBold",
+  SG_Bold: "SpaceGrotesk_700Bold",
+};
+
+export default AppFonts;
