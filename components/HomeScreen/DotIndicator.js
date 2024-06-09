0c150381db872d93bc16e84c5e2717e6109cc174
Rohan Bimal Raj <rohanbimalraj@gmail.com>
2024-06-09 19:55:26 +0530

    Removed old carousel indicator

--- components/HomeScreen/DotIndicator.js
+++ components/HomeScreen/DotIndicator.js
@@ -1,20 +1,10 @@
 import { View, StyleSheet } from "react-native";
-import Dots from "react-native-dots-pagination";
 import Colors from "../../constants/colors";
 import { PageIndicator } from 'react-native-page-indicator';
 
 function DotIndicator({ length, currentIndex }) {
   return (
     <View style={styles.dotContainer}>
-      {/* <Dots
-        length={length}
-        active={currentIndex}
-        width={200}
-        passiveColor="#ccc"
-        activeColor={Colors.accent600}
-        activeDotHeight={13}
-        activeDotWidth={13}
-      /> */}
       <PageIndicator count={length} current={currentIndex} color="#ccc" activeColor={Colors.accent600}/>
     </View>
   );
