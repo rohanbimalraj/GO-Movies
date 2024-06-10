1e615fe72ed8237399ca1bae6535114d908b74c4
Rohan Bimal Raj <rohanbimalraj@gmail.com>
2024-06-10 18:38:03 +0530

    Added loader for images

--- components/HomeScreen/CarouselItem.js
+++ components/HomeScreen/CarouselItem.js
@@ -5,17 +5,24 @@ import {
   ImageBackground,
   StyleSheet,
   Dimensions,
+  ActivityIndicator
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import AppFonts from "../../constants/app-fonts";
 import Colors from "../../constants/colors";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { useState } from "react";
 
 const BASE_URL = "https://image.tmdb.org/t/p/w780";
 const width = Dimensions.get("window").width;
 
 function CarouselItem({ id, title, backDropPath }) {
   const navigation = useNavigation();
+  const [loading, setLoading] = useState(true);
+
+  function onLoad() {
+    setLoading(false);
+  }
   function onPressHandler() {
     console.log("Movie Id: ", id);
     navigation.navigate("Details");
@@ -27,11 +34,17 @@ function CarouselItem({ id, title, backDropPath }) {
           style={styles.container}
           source={{ uri: BASE_URL + backDropPath }}
           imageStyle={styles.backDroup}
+          onLoad={onLoad}
         >
           <LinearGradient
             colors={["#00000000", "#000000"]}
             style={[styles.container]}
           >
+            {loading && (
+              <View style={styles.loader}>
+                <ActivityIndicator size="large" color={Colors.primary600} />
+              </View>
+            )}
             <View style={styles.titleContainer}>
               <Text style={styles.title}>{title}</Text>
             </View>
@@ -65,5 +78,15 @@ const styles = StyleSheet.create({
     fontFamily: AppFonts.SG_Bold,
     fontSize: 18,
     color: Colors.accent500,
+  },
+    loader: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.5)', // semi-transparent white background
   },
 });
--- components/HomeScreen/MoviePoster.js
+++ components/HomeScreen/MoviePoster.js
@@ -1,13 +1,33 @@
-import { View, Image, StyleSheet, Pressable, Dimensions } from "react-native";
+import {
+  View,
+  Image,
+  StyleSheet,
+  Pressable,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
+import { useState } from "react";
+import Colors from "../../constants/colors";
 
 const BASE_URL = "https://image.tmdb.org/t/p/w342";
 
 function MoviePoster({ id, posterPath }) {
+  const [loading, setLoading] = useState(true);
+
+  function onLoad() {
+    setLoading(false);
+  }
   return (
     <View style={styles.rootContainer}>
+      {loading && (
+        <View style={styles.loader}>
+          <ActivityIndicator size="large" color={Colors.primary600} />
+        </View>
+      )}
       <Image
         source={{ uri: BASE_URL + posterPath }}
         style={styles.image}
+        onLoad={onLoad}
       />
     </View>
   );
@@ -24,7 +44,18 @@ const styles = StyleSheet.create({
   },
   image: {
     width: width / 3,
-    height: 1.5 * width/3,
+    height: (1.5 * width) / 3,
     borderRadius: 10,
+  },
+  loader: {
+    borderRadius: 10,
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.5)", // semi-transparent white background
   },
 });
