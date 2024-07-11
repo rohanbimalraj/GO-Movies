aad74ba12c21d3fe72c632dd62d718bd2042b26e
Rohan Bimal Raj <rohanbimalraj@gmail.com>
2024-07-11 08:01:30 +0530

    Removed access token

--- utils/https.js
+++ utils/https.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/movi'
 // Replace with your token retrieval
-axios.defaults.headers.common['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNTQxNzBmMmNhZGQ0MTgzN2I5MWJhMWM2NzA5OTI4MyIsIm5iZiI6MTcyMDM3MDQ5Ni40NDE5NjMsInN1YiI6IjYxNGZlYmMwMmQ4ZWYzMDA0MjFjM2NiNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.92IoY9r92XIeB_QlcLavBefiHj2W0j8-4_eV6y1iUwM';
+axios.defaults.headers.common['Authorization'] = 'Bearer "ACCESS TOKEN"';
 
 export async function fetchNowPlayingMovies() {
     try {
